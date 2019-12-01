272aeb9b816157e47991128ce0040027e6e7c2e4
Jin YeongHwa <33739495+jyh2378@users.noreply.github.com>
2019-12-01 18:22:59 +0900

    Update Graph.cpp

--- Data_structure/Graph.cpp
+++ Data_structure/Graph.cpp
@@ -6,46 +6,46 @@
 
 using namespace std;
 
-class vertex {
+class Vertex {
 public:
 	int data;
 	int degree;
 	bool visited; // check vertex is visited or not by DFS
-	vector<vertex*> adj_list;
+	vector<Vertex*> adj_list;
 
-	vertex(int data) {
+	Vertex(int data) {
 		this->data = data;
 		this->degree = 0;
 		this->visited = false;
 	}
 };
 
-class edge {
+class Edge {
 public:
-	vertex* src;
-	vertex* dst;
+	Vertex* src;
+	Vertex* dst;
 	int data;
 	bool edge_stat; // edge status
 
-	edge(vertex* src, vertex* dst) {
+	Edge(Vertex* src, Vertex* dst) {
 		this->src = src;
 		this->dst = dst;
 		this->edge_stat = NOT_EXPLORED;
 	}
 };
 
-class graph {
+class Graph {
 private:
-	vector<vertex*> vertex_list;
-	vector<edge*> edge_list;
+	vector<Vertex*> vertex_list;
+	vector<Edge*> edge_list;
 	int v_sz;
 public:
-	graph() {
+	Graph() {
 		this->v_sz = 0;
 	}
 
-	vertex* findvertex(int data) {
-		vertex* v = NULL;
+	Vertex* findvertex(int data) {
+		Vertex* v = NULL;
 		for (int i = 0; i < vertex_list.size(); i++) {
 			if (vertex_list[i]->data == data) {
 				v = vertex_list[i];
@@ -55,8 +55,8 @@ public:
 		return v;
 	}
 
-	edge* findedge(vertex* src, vertex* dst) {
-		edge* e = NULL;
+	Edge* findedge(Vertex* src, Vertex* dst) {
+		Edge* e = NULL;
 		for (int i = 0; i < edge_list.size(); i++) {
 			if (edge_list[i]->src == src && edge_list[i]->dst == dst) {
 				e = edge_list[i];
@@ -68,35 +68,38 @@ public:
 
 	void insert_vertex(int data) {
 		if (findvertex(data) == NULL) {
-			vertex* new_v = new vertex(data);
+			Vertex* new_v = new Vertex(data);
 			vertex_list.push_back(new_v);
 		}
 	}
 
 	void insert_edge(int src_data, int dst_data) {
-		vertex* src = findvertex(src_data);
-		vertex* dst = findvertex(dst_data);
+		Vertex* src = findvertex(src_data);
+		Vertex* dst = findvertex(dst_data);
 		if (findedge(src, dst) == NULL) {
-			edge* new_e = new edge(src, dst);	// generate edge 
+			Edge* new_e = new Edge(src, dst);	// generate edge 
 			edge_list.push_back(new_e);
 		}
 
 		src->adj_list.push_back(dst);			// insert vertex to their adj list
-		dst->adj_list.push_back(src);
 	}
 
-	void dfs(vertex* v) {
+	void dfs(Vertex* v) {
 		if (v->visited) return;
 		v->visited = true;
+		cout << v->data << " ";
 		for (int i = 0; i < v->adj_list.size(); i++) { // check adjacency list
-			vertex* next_v = v->adj_list[i];
-			edge* e = findedge(v, next_v);
+			Vertex* next_v = v->adj_list[i];
+			Edge* e1 = findedge(v, next_v);
+			Edge* e2 = findedge(next_v, v);
 			if (next_v->visited == false) { // if next vertex isn't visited
-				e->edge_stat = DISCOVERY;
+				e1->edge_stat = DISCOVERY;
+				e2->edge_stat = DISCOVERY;
 				dfs(next_v);
 			}
 			else { // if next vertex is visited
-				e->edge_stat = BACK;
+				e1->edge_stat = BACK;
+				e2->edge_stat = BACK;
 			}
 		}
 		return;
